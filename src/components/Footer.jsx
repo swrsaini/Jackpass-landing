463c8f7f808f0f51a0e0642a4f0a132354c20960
Shubham Saini <Shubhamsaini98465@gmail.com>
2025-01-21 00:28:35 +0530

    improved footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,36 @@
 import { Instagram, Twitter, Facebook, Linkedin, Youtube } from "lucide-react";
-import jackpassIcon from '/images/jackpassIcon.png'
-import jackpassText from '/images/jackpassText.png'
+import jackpassIcon from "/images/jackpassIcon.png";
+import jackpassText from "/images/jackpassText.png";
 export function Footer() {
+  const social = [Instagram, Twitter, Facebook, Linkedin, Youtube];
+  const product = [
+    "Features",
+    "Communities",
+    "Events",
+    "Mobile App",
+    "Pricing",
+  ];
+  const company = ["About Us", "Careers", "Press", "Blog", "Contact"];
+  const support = [
+    "Help Center",
+    "Safety Center",
+    "Community Guidelines",
+    "Terms of Service",
+    "Privacy Policy",
+  ];
+  const footerItem = [
+    { name: "Product", items: product },
+    { name: "Company", items: company },
+    { name: "Support", items: support }
+  ];
   return (
     <footer className="bg-white border-t border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12">
         <div className="grid grid-cols-3 md:grid-cols-6  gap-8 mb-8 md:mb-12">
           <div className="col-span-3">
             <div className="flex h-8 mb-2">
-              <img
-                src={jackpassIcon}
-                alt="icon"
-              />
-              <img
-                className="h-8"
-                src={jackpassText}
-                alt=""
-              />
+              <img src={jackpassIcon} alt="icon" />
+              <img className="h-8" src={jackpassText} alt="" />
             </div>
 
             <p className="text-gray-600 mb-6 max-w-md">
@@ -24,128 +38,36 @@ export function Footer() {
               interests. Discover events, join groups, and make new friends.
             </p>
             <div className="flex gap-4">
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-600 transition-colors"
-              >
-                <Instagram className="w-5 h-5" />
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-600 transition-colors"
-              >
-                <Twitter className="w-5 h-5" />
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-600 transition-colors"
-              >
-                <Facebook className="w-5 h-5" />
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-600 transition-colors"
-              >
-                <Linkedin className="w-5 h-5" />
-              </a>
-              <a
-                href="#"
-                className="text-gray-400 hover:text-gray-600 transition-colors"
-              >
-                <Youtube className="w-5 h-5" />
-              </a>
+              {social.map((Icon, index) => {
+                return (
+                  <div key={index}>
+                    <a
+                      href="#"
+                      className="text-gray-400 hover:text-gray-600 transition-colors"
+                    >
+                      <Icon className="w-5 h-5" />
+                    </a>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div>
-            <h4 className="font-semibold text-gray-900 mb-4">Product</h4>
-            <ul className="text-sm space-y-3">
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Communities
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Events
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Mobile App
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Pricing
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="font-semibold text-gray-900 mb-4">Company</h4>
-            <ul className="text-sm  space-y-3">
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Careers
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Press
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h4 className="font-semibold text-gray-900 mb-4">Support</h4>
-            <ul className="text-sm space-y-3">
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Help Center
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Safety Center
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Community Guidelines
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Terms of Service
-                </a>
-              </li>
-              <li>
-                <a href="#" className="text-gray-600 hover:text-gray-900">
-                  Privacy Policy
-                </a>
-              </li>
-            </ul>
-          </div>
+          {footerItem.map((e, index) => {
+                return <div key={index}> 
+                <h4 className="font-semibold text-gray-900 mb-4">{e.name}</h4>
+                <ul className="text-sm space-y-3">
+                
+                  {
+                   e.items.map((el,index) => {
+                    return <li key={index}>
+                    <a href="#" className="text-gray-600 hover:text-gray-900">
+                      {el}
+                    </a>
+                  </li>
+                  })}
+                </ul>
+              </div>
+              })}
         </div>
         <div className="pt-8 border-t border-gray-200">
           <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
